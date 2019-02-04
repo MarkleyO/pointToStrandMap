cce12b436e2d62f63ca10abb3dbd7a792ae4b0b2
Owen Markley <markleyo@oregonstate.edu>
2019-02-03 17:48:40 -0800

    trying to fix icons

--- js/main.js
+++ js/main.js
@@ -8,14 +8,23 @@ var mymap = L.map('map', {
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
 
-var airPorts = null;
-airPorts = L.geoJson.ajax("assets/airports.geojson", {
-  attribution: 'AirPorts | Made By: Owen Markley'
-});
-airPorts.addTo(mymap);
+//var airPorts = null;
+// airPorts = L.geoJson.ajax("assets/airports.geojson", {
+//   attribution: 'AirPorts | Made By: Owen Markley'
+// });
+//airPorts.addTo(mymap);
 
-var colors = chroma.scale('Dark2').mode('1ch').colors(9);
+L.geoJson.ajax("assets/airports.geojson", {
+  onEachFeature: function (feature, layer){
+    layer.bindPopup(feature.propeties.AIRPT_NAME);
+  },
+  pointToLayer: function (feature, latlng) {
+    console.log("here");
+    if(feature.properties.CNTL_TWR == "Y")//there is a tower
+      return L.marker(latlng, {icon: L.divIcon({classname: 'fa fa-broadcast-tower'})});
+    else//there is no tower
+      return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-plane-departure'})});
+  }
+}).addTo(mymap);
 
-for(i=0; i<9; i++){
-  $('head').append($("<style> .market-color-" + (i+1).toString() + " {color: " + colors[i] + "; font-size: 15px; text-shadow: 0 0 3px #ffffff;} </style>"));
-}
+L.geoJson.ajax("assets/us-states.geojson").addTo(mymap);
