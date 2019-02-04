155abfc99bfc114633d8e5d2f0a02042b6ef870f
Owen Markley <markleyo@oregonstate.edu>
2019-02-03 19:57:04 -0800

    final update?

--- js/main.js
+++ js/main.js
@@ -6,25 +6,72 @@ var mymap = L.map('map', {
   detectRetina: true
 });
 
-L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(mymap);
+L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
+    attribution: 'Airport Tower Data &copy; Mike Bostock (D3) | United States &copy; usgs dataset | Base Map &copy; CartoDB'
+}).addTo(mymap);
 
-//var airPorts = null;
-// airPorts = L.geoJson.ajax("assets/airports.geojson", {
-//   attribution: 'AirPorts | Made By: Owen Markley'
-// });
-//airPorts.addTo(mymap);
+var color = null;
 
-L.geoJson.ajax("assets/airports.geojson", {
+var airPorts = null;
+
+airPorts = L.geoJson.ajax("assets/airports.geojson", {
   onEachFeature: function (feature, layer){
-    layer.bindPopup(feature.propeties.AIRPT_NAME);
+    layer.bindPopup(feature.properties.AIRPT_NAME);
   },
   pointToLayer: function (feature, latlng) {
-    console.log("here");
     if(feature.properties.CNTL_TWR == "Y")//there is a tower
-      return L.marker(latlng, {icon: L.divIcon({classname: 'fa fa-broadcast-tower'})});
+      return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-broadcast-tower fa-lg'})});
     else//there is no tower
-      return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-plane-departure'})});
+      return L.marker(latlng, {icon: L.divIcon({className: 'fa fa-plane-departure fa-lg'})});
   }
 }).addTo(mymap);
 
-L.geoJson.ajax("assets/us-states.geojson").addTo(mymap);
+colors = chroma.scale('Purples').mode('hsl').colors(5);
+
+function setColor(density){
+  var id=0;
+  if(density>16){id=4;}
+  else if(density>12 && density<=16){id=3;}
+  else if(density>8 && density<=12){id=2;}
+  else if(density>4 && density<=8){id=1;}
+  else{id=0;}
+  return colors[id];
+}
+
+function style(feature){
+  return{
+    fillColor: setColor(feature.properties.count),
+    fillOpacity: 0.4,
+    weight: 2,
+    opacity: 1,
+    color: '#ffffff',
+    dashArray: '4'
+  };
+}
+
+L.geoJson.ajax("assets/us-states.geojson",{
+  style: style
+}).addTo(mymap);
+
+var legend = L.control({position: 'topright'});
+
+legend.onAdd = function(){
+
+  var div = L.DomUtil.create('div', 'legend');
+
+  div.innerHTML += '<b># Airports</b><br />';
+  div.innerHTML += '<i style="background: ' + colors[4] + '; opacity: 0.5"></i><p>17+</p>';
+  div.innerHTML += '<i style="background: ' + colors[3] + '; opacity: 0.5"></i><p>13-16</p>';
+  div.innerHTML += '<i style="background: ' + colors[2] + '; opacity: 0.5"></i><p>9-12</p>';
+  div.innerHTML += '<i style="background: ' + colors[1] + '; opacity: 0.5"></i><p> 5-8</p>';
+  div.innerHTML += '<i style="background: ' + colors[0] + '; opacity: 0.5"></i><p> 0- 4</p>';
+  div.innerHTML += '<hr><b>Has Broadcast Tower?<b><br />';
+  div.innerHTML += '<i class="fa fa-broadcast-tower marker-color-1"></i><p> Yes</p>';
+  div.innerHTML += '<i class="fa fa-plane-departure marker-color-2"></i><p> No</p>';
+
+  return div;
+};
+
+legend.addTo(mymap);
+
+L.control.scale({position: 'bottomleft'}).addTo(mymap);
